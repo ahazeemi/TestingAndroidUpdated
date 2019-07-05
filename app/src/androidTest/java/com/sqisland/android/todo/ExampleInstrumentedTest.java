b8e91d3085e1f85de9de439d19cb1905665a7490
ahazeemi <abdulhameed.azeemi@hotmail.com>
2019-07-05 13:53:38 +0500

    Change package name

--- app/src/androidTest/java/com/sqisland/android/todo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/sqisland/android/todo/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.sqisland.android.hello", appContext.getPackageName());
+        assertEquals("com.sqisland.android.todo", appContext.getPackageName());
     }
 }
